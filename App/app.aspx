fc5b6aae3dbaf39284d2524df70ce117ac4d05ba
Bernard Mantha <Bernard.Mantha@gmail.com>
2016-08-09 07:44:55 -0400

    Added app.js

---
+++ App/app.aspx
@@ -0,0 +1,13 @@
+<H1>SP Tree Map</H1>
+<div id="TreeNodeMapContainer" ng-app="sptreemap">
+    <div ui-view="header"></div>
+    <div ui-view="content"></div>
+    <div ui-view="footer"></div>
+</div>
+
+<!-- build:css styles/main.css -->
+
+<!-- endbuild -->
+
+
+<script src="/scripts/app.js"></script>
--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,103 @@
 var gulp = require('gulp');
 var spsave = require("gulp-spsave");
 var settings = require("./settings.js");
+var minifycss = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    stylish = require('jshint-stylish'),
+    uglify = require('gulp-uglify'),
+    usemin = require('gulp-usemin'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    changed = require('gulp-changed'),
+    rev = require('gulp-rev'),
+    browserSync = require('browser-sync'),
+    del = require('del');
+
+gulp.task('jshint', function() {
+  return gulp.src('app/scripts/**/*.js')
+  .pipe(jshint())
+  .pipe(jshint.reporter(stylish));
+});
+
+// Clean
+gulp.task('clean', function() {
+    return del(['dist']);
+});
+
+gulp.task('usemin',['jshint'], function () {
+  return gulp.src('./app/app.aspx')
+      .pipe(usemin({
+        css:[minifycss()],
+        js: [uglify()]
+      }))
+      .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('nakedcopy',function () {
+  return gulp.src('app/scripts/**/*.js')
+      .pipe(gulp.dest('dist/'));
+});
+
+// Images
+gulp.task('imagemin', function() {
+  return del(['dist/images']), gulp.src('app/images/**/*')
+    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('dist/images'))
+    .pipe(notify({ message: 'Images task complete' }));
+});
+
+gulp.task('copyfonts', ['clean'], function() {
+   gulp.src('./bower_components/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*')
+   .pipe(gulp.dest('./dist/fonts'));
+   gulp.src('./bower_components/bootstrap/dist/fonts/**/*.{ttf,woff,eof,svg}*')
+   .pipe(gulp.dest('./dist/fonts'));
+});
+
+// Default task
+gulp.task('default', ['clean'], function() {
+  //  gulp.start('usemin', 'imagemin','copyfonts');
+   gulp.start('nakedcopy', 'imagemin','copyfonts');
+});
+
+gulp.task('watch', ['browser-sync'], function() {
+  // Watch .js files
+  gulp.watch('{app/scripts/**/*.js,app/styles/**/*.css,app/**/*.html}', ['usemin']);
+      // Watch image files
+  gulp.watch('app/images/**/*', ['imagemin']);
+
+});
+
+gulp.task('browser-sync', ['default'], function () {
+   var files = [
+      'app/**/*.html',
+      'app/**/*.aspx',
+      'app/styles/**/*.css',
+      'app/images/**/*.png',
+      'app/scripts/**/*.js',
+      'dist/**/*'
+   ];
+
+   browserSync.init(files, {
+      server: {
+         baseDir: "dist",
+         index: "menu.html"
+      }
+   });
+        // Watch any files in dist/, reload on change
+  gulp.watch(['dist/**']).on('change', browserSync.reload);
+    });
+
 gulp.task("spsave", function () {
-    return gulp.src(["App/*.*"])
+    return gulp.src(["dist/**/*.*"])
         .pipe(spsave({
             siteUrl: settings.siteUrl,
             username: settings.username,
             //domain: settings.domain,
             password: settings.password,
-            folder: "SiteAssets",
-            flatten: true
+            folder: "SiteAssets/jsapps/sptreemap",
+            flatten: false
         }));
 });
